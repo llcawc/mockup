b68997a7daea646f5a9bce1645906cd87f5d7d10
llcawc <hostmastername@gmail.com>
2023-01-05 23:48:17 +0300

    up230105

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -2,39 +2,44 @@
 
 // import modules
 import { env } from 'node:process'
+import gulp from 'gulp'
+const { src, dest, parallel, series, watch } = gulp
 import { rollup } from 'rollup'
 import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
-import chalk from 'chalk'
+import { minify } from "terser"
+import gulpTerser from "gulp-terser"
 
 // variables & path
 const baseDir = 'src' // Base directory path without «/» at the end
 const distDir = 'dist' // Distribution folder for uploading to the site
 let paths = {
-  src: baseDir + '/assets/scripts/main.js',
-  dest: distDir + '/assets/js/main.min.js',
+  src:  baseDir + '/assets/scripts/main.js',
+  min:  distDir + '/assets/js/main.min.js',
+  dest: distDir + '/assets/js',
 }
 
 // task
-export async function scripts() {
+async function compile() {
   const bundle = await rollup({
     input: paths.src,
     plugins: [nodeResolve(), commonjs({ include: 'node_modules/**' }), babel({ babelHelpers: 'bundled' })],
   })
-
   await bundle.write({
-    file: paths.dest,
+    file: paths.min,
     format: 'iife',
     name: 'main',
     sourcemap: env.BUILD === 'production' ? false : true,
-    plugins: env.BUILD === 'production' ? [terser({compress: {passes: 2}, format: {comments: false}})] : false,
   })
-
-  if (env.BUILD === 'production') {
-    console.log(chalk.green('JS build for production is completed OK!'))
-  } else {
-    console.log(chalk.magenta('Script developments is running OK!'))
-  }
 }
+
+// minify scripts task
+function min() {
+  return src(paths.min)
+    .pipe(gulpTerser({compress: {passes: 2}, format: {comments: false}}, minify))
+    .pipe(dest(paths.dest))
+}
+
+// export
+export let scripts = env.BUILD === 'production' ? series(compile, min) : compile
