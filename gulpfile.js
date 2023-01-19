b0d3aeb079498617c23d5bb92bc3cd1fd1cfb3bd
llcawc <hostmastername@gmail.com>
2023-01-19 21:37:55 +0300

    up230119

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ import { clean, assetscopy } from './gulp/assets.js'
 // variables & path
 const baseDir = 'src' // Base directory path without «/» at the end
 const distDir = 'dist' // Distribution folder for uploading to the site
-const fileswatch = 'html,htm,css,php,txt,js,cjs,mjs,webp,jpg,png,svg,json,md,woff2'
+const fileswatch = 'html,htm,njk,scss,sass,css,php,txt,js,cjs,mjs,webp,jpg,png,svg,json,md,woff2'
 
 //  server reload task
 function browserSync() {
@@ -27,7 +27,7 @@ function browserSync() {
 
 // watch task
 function watchDev() {
-  watch(`./${baseDir}/**/*.html`, { usePolling: true }, htmlbau)
+  watch(`./${baseDir}/**/*.{html,htm,njk}`, { usePolling: true }, htmlbau)
   watch(`./${baseDir}/assets/scripts/**/*.{js,mjs,cjs}`, { usePolling: true }, scripts)
   watch(`./${baseDir}/assets/sass/**/*.{scss,sass,css}`, { usePolling: true }, styles)
   watch(`./${baseDir}/assets/images/**/*.{jpg,png,svg}`, { usePolling: true }, images)
